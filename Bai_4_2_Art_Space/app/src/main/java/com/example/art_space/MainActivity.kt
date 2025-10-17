6a08786ce6a7ff377d3e602192ba7dc1ac28d9d0
tuananhwtpq <tuananhwtpq@gmail.com>
2025-10-17 16:56:20 +0700

    17/10 Finish Art Space app

--- Bai_4_2_Art_Space/app/src/main/java/com/example/art_space/MainActivity.kt
+++ Bai_4_2_Art_Space/app/src/main/java/com/example/art_space/MainActivity.kt
@@ -19,7 +19,9 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
@@ -82,6 +84,7 @@ fun ArtSpace(modifier: Modifier = Modifier) {
     Column(
         modifier = modifier
             .fillMaxSize()
+            .verticalScroll(rememberScrollState())
             .padding(12.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
